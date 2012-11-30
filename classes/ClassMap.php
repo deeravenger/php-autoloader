acf25744ffd054f0d8e713de2d767b50e33501e6
dmkuznetsov <dev.kuznetsov@gmail.com>
2012-12-01 00:19:54 +0400

    update licence

--- classes/ClassMap.php
+++ classes/ClassMap.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * ClassMap
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright (c) 2012, Dmitry Kuznetsov <dev.kuznetsov@gmail.com>. All rights reserved.
+ * @author Dmitry Kuznetsov <dev.kuznetsov@gmail.com>
+ * @url https://github.com/dmkuznetsov/php-class-map
+*/
+
 if ( !defined( 'T_ML_COMMENT' ) )
 {
 	define( 'T_ML_COMMENT', T_COMMENT );
@@ -8,10 +29,6 @@ else
 	define( 'T_DOC_COMMENT', T_ML_COMMENT );
 }
 
-/**
- * @author Dmitry Kuznetsov 2012
- * @url https://github.com/dmkuznetsov/php-class-map
- */
 class ClassMap
 {
 	/**
--- map.php
+++ map.php
@@ -1,10 +1,24 @@
 <?php
 /**
- * Usage:
- * php map.php --help
- * @author Dmitry Kuznetsov 2012
+ * ClassMap
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright (c) 2012, Dmitry Kuznetsov <dev.kuznetsov@gmail.com>. All rights reserved.
+ * @author Dmitry Kuznetsov <dev.kuznetsov@gmail.com>
  * @url https://github.com/dmkuznetsov/php-class-map
- */
+*/
 require_once dirname( __FILE__ ) . '/classes/ClassMap.php';
 require_once dirname( __FILE__ ) . '/classes/Writer.php';
 require_once dirname( __FILE__ ) . '/classes/Log.php';
