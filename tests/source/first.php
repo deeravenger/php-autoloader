7ec8e2b46577e6a72e0ceddd10c090945dc328d7
dmkuznetsov <dev.kuznetsov@gmail.com>
2013-02-26 23:47:18 +0400

    add tests

---
+++ tests/source/first.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Glob {
+	abstract class Fifa
+	{}
+
+	final
+	class /** 123 */
+	Story
+	{}
+
+}
+
+
+
+
+namespace    /*so, its great*/ /*yes*/ Bar    {
+
+
+define( 'TTT', 1 );
+
+
+
+
+class OtherClass
+{}
+
+	TTT;
+	class AABBC
+	{}
+
+
+//namespace Foo;
+class Test
+{
+
+}
+
+interface   /* хохоххо */   TestInterface
+{}
+}
+
+namespace /*eew*/ /*ddw*/ {
+	TTT;
+class EmptyClass
+{}
+}
